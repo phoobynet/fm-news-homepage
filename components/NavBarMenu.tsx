d173838c08391eb1d7a052c673cc22aa3c23c197
Glenn <phoobynet@users.noreply.github.com>
2023-01-05 21:33:35 +0000

    wip

--- components/NavBarMenu.tsx
+++ components/NavBarMenu.tsx
@@ -1,9 +1,5 @@
 import styles from './NavBarMenu.module.scss'
 
 export default function NavBarMenu() {
-  return (
-    <div className={styles['navbar-menu']}>
-      TODO: Menu
-    </div>
-  )
+  return <div className={styles.navbarMenu}></div>
 }
