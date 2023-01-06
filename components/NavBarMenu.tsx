ed866f95b155495cf251f838403b7d4d2a1675d2
Glenn <phoobynet@users.noreply.github.com>
2023-01-06 16:24:44 +0000

    Fuck off!

--- components/NavBarMenu.tsx
+++ components/NavBarMenu.tsx
@@ -2,23 +2,18 @@ import styles from './NavBarMenu.module.scss'
 import NavBarMenuHamburger from '@/components/NavBarMenuHamburger'
 import { useMenuStore } from '@/stores/useMenuStore'
 import { motion } from 'framer-motion'
-import { useEffect, useRef } from 'react'
 import { createPortal } from 'react-dom'
+
+const menuItems = ['Home', 'New', 'Popular', 'Trending', 'Categories']
 
 interface ClickableMenuItemProps {
   onMenuItemClick: (item: string) => void
 }
 
-const MenuItems = ({ onMenuItemClick }: ClickableMenuItemProps) => {
-  const items = ['Home', 'New', 'Popular', 'Trending', 'Categories']
-  return (
-    <motion.ul
-      className={styles.menuItems}
-      initial={{ width: 0 }}
-      animate={{ width: 256 }}
-      transition={{ duration: 0.2 }}
-    >
-      {items.map((item) => (
+const Desktop = ({ onMenuItemClick }: ClickableMenuItemProps) => (
+  <div className={styles.desktopMenuContainer}>
+    <ul className={styles.menuItems}>
+      {menuItems.map((item) => (
         <li
           key={item}
           onClick={() => onMenuItemClick(item)}
@@ -26,31 +21,12 @@ const MenuItems = ({ onMenuItemClick }: ClickableMenuItemProps) => {
           {item}
         </li>
       ))}
-    </motion.ul>
-  )
-}
-
-const Desktop = ({ onMenuItemClick }: ClickableMenuItemProps) => (
-  <div className={styles.desktopMenuContainer}>
-    <MenuItems onMenuItemClick={onMenuItemClick} />
+    </ul>
   </div>
 )
 
 const Mobile = ({ onMenuItemClick }: ClickableMenuItemProps) => {
   const menuOpen = useMenuStore((state) => state.menuOpen)
-  const menuContainerRef = useRef<HTMLElement | null>(null)
-
-  useEffect(() => {
-    menuContainerRef.current = document.getElementById('menu-container')
-  }, [])
-
-  useEffect(() => {
-    // TODO: add styling to menu container
-  }, [menuOpen])
-
-  if (!menuContainerRef.current) {
-    return null
-  }
 
   return (
     <div className={styles.mobileMenuContainer}>
@@ -64,9 +40,23 @@ const Mobile = ({ onMenuItemClick }: ClickableMenuItemProps) => {
               animate={{ opacity: 0.5 }}
               transition={{ duration: 0.2 }}
             />
-            <MenuItems onMenuItemClick={onMenuItemClick} />
+            <motion.ul
+              className={styles.menuItems}
+              initial={{ width: 0 }}
+              animate={{ width: 256 }}
+              transition={{ duration: 0.2 }}
+            >
+              {menuItems.map((item) => (
+                <li
+                  key={item}
+                  onClick={() => onMenuItemClick(item)}
+                >
+                  {item}
+                </li>
+              ))}
+            </motion.ul>
           </>,
-          menuContainerRef.current,
+          document.getElementById('menu-container') as HTMLElement,
         )}
     </div>
   )
